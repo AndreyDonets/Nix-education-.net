3582b042f439230a922139a60529f90cafea6560
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-09-28 00:22:01 +0300

    Commit 3: added classes Circle and Rectangle inherited from abstract class Point

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    public class Circle : Point
+    {
+        public Circle(PointF center, float radius)
+        {
+            Coordinates = center;
+            if (radius <= 0)
+                throw new ArgumentException("Radius cannot be less than or equal to zero");
+            this.Radius = radius;
+        }
+        public float Radius { get; protected set; }
+        public override void Move(float x, float y) => Coordinates += new SizeF(x, y);
+        public override void Print() => Console.WriteLine($"Circle.\nCenter of circle: x = {Coordinates.X}, y = {Coordinates.Y}\n Radius = {Radius}");
+        public override void ToScale(float scale)
+        {
+            Coordinates = new PointF(Coordinates.X * scale, Coordinates.Y * scale);
+            Radius = Radius * scale;
+        }
+    }
+}
