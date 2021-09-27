190f4cb5e7d69998da0a98849170df496fdcf4d1
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-09-28 00:23:23 +0300

    Commit 4: added class Triangle inherited from class Rectangle

---
+++ Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    public class Triangle : Rectangle
+    {
+        public Triangle(PointF basePoint, PointF shiftToSecondCorner, PointF shiftToThirdCorner) : base(basePoint, shiftToSecondCorner)
+        {
+            this.ShiftToThirdCorner = shiftToThirdCorner;
+        }
+        public PointF ShiftToThirdCorner { get; protected set; }
+        public override void Print() => Console.WriteLine($"Triangle.\nSides of the Triangle:\n\tA:X = {Coordinates.X}, A:Y = {Coordinates.Y}\n\tB:X = {Coordinates.X+Shift.X}, B:Y = {Coordinates.Y+Shift.Y}\n\tC:X = {Coordinates.X+ShiftToThirdCorner.X}, C:Y = {Coordinates.Y+ShiftToThirdCorner.Y}");
+        public override void ToScale(float scale)
+        {
+            base.ToScale(scale);
+            ShiftToThirdCorner = new PointF(ShiftToThirdCorner.X * scale, ShiftToThirdCorner.Y * scale);
+        }
+    }
+}
