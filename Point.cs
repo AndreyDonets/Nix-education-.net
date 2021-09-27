886b01b1b2239e80d4973f0372d64b26f23195dc
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-09-28 00:19:42 +0300

    commit 2: add base class Point

---
+++ Point.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp1
+{
+    public abstract class Point
+    {
+        public PointF Coordinates { get; protected set; }
+        public abstract void Print(); 
+        // public abstract string Print();
+        public abstract void Move(float x, float y);
+        public abstract void ToScale(float scale);
+    }
+}
